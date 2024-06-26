8b446870d5abd0b9f0e09b16c67812871c85cccc
Kateryna Huk <khuk@c4r5p8.42vienna.com>
2024-06-26 23:32:27 +0200

    26.06.2024 the fill_info functions added

--- fill_info.c
+++ fill_info.c
@@ -12,3 +12,84 @@
 
 #include "push_swap.h"
 
+int	ind_of_next(int *arr, int size, int nb)
+{
+	int	res;
+	int	i;
+	int index;
+	int check;
+
+	
+	i = 0;
+	res = 0;
+	while (i < size)
+	{
+		res = arr[i] - nb;
+		if (res)
+	}
+}
+
+inf_node	*find_closest(int *arr_a, int *arr_b, int size_a, int size_b)
+{
+	int			i;
+	inf_node	*calc;
+
+	calc = (inf_node *)malloc(sizeof(inf_node) * size_b);
+	if (!calc)
+		return (NULL);
+	i = 0;
+	while (i < size_b)
+	{
+		calc[i].ind_clos = ind_of_next(arr_a, size_a, arr_b[i]);
+		i++;
+	}
+	return (calc);
+}
+
+void	fill_one(inf_node *inf_arr, int *stack_a, int size_arr)
+{
+	int i;
+
+	i = 0;
+	while (i < size_arr)
+	{
+		inf_arr[i].closest = stack_a[inf_arr[i].ind_clos];
+		i++;
+	}
+	i = 0;
+	while (i < size_arr)
+	{
+		if (i < (size_arr / 2))
+			inf_arr[i].nb_ontop = i;
+		else
+			inf_arr[i].nb_ontop = size_arr - i;
+		i++;
+	}
+}
+
+void	fill_two(inf_node *inf_arr, int *stack_b, int size_arr)
+{
+	int	i;
+
+	i = 0;
+	while (i < size_arr)
+	{
+		inf_arr[i].nb = stack_b[i];
+		i++;
+	}
+	i = 0;
+	while (i < size_arr)
+	{
+		if (inf_arr[i].ind_clos < (size_arr / 2))
+			inf_arr[i].clos_ontop = inf_arr[i].ind_clos;
+		else
+			inf_arr[i].clos_ontop = size_arr - inf_arr[i].ind_clos;
+		i++;
+	}
+	i = 0;
+	while (i < size_arr)
+	{
+		inf_arr[i].res_mvs = inf_arr[i].clos_ontop + inf_arr[i].nb_ontop;
+		i++;
+	}
+}
--- main.c
+++ main.c
@@ -35,7 +35,15 @@ int	main(int ac, char *av[])
 	for (int i = 0; i < size_1; i++)
 		ft_printf("%i\n", my_stack[i]);
 	ft_printf("---------------------\n");
+	inf_node *inf = find_closest(my_stack, my_stack2, size_1, size_2);
+	int size_arr = size_2;
+	fill_one(inf, my_stack, size_arr);
+	fill_two(inf, my_stack2, size_arr);
+	for (int i = 0; i < size_arr; i++)
+		ft_printf("nb:%i, ind_of_clos:%i, clos:%i, res_mvs:%i\n", inf[i].nb, inf[i].ind_clos, inf[i].closest, inf[i].res_mvs);
+	ft_printf("---------------------\n");
 	free (my_stack);
 	free (my_stack2);
+	free (inf);
 	return (0);
 }
