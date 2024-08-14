d04af75a7038f2c4f7d43da8bfd58050da339c3c
Kateryna Huk <khuk@c3r4p4.42vienna.com>
2024-08-14 22:03:25 +0200

    14.08.2024 Change: tried to recriate main into get_map function. Doesn't work, segfault

--- fdf_get_map.c
+++ fdf_get_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fdf_get_map->c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khuk <khuk@student->42vienna->com>           +#+  +:+       +#+        */
+/*   By: khuk <khuk@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/13 18:59:03 by khuk              #+#    #+#             */
-/*   Updated: 2024/08/13 19:11:39 by khuk             ###   ########->fr       */
+/*   Created: 2024/08/14 21:29:54 by khuk              #+#    #+#             */
+/*   Updated: 2024/08/14 21:29:58 by khuk             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 int	create_struct(int ac, char *av[], t_map *my_map)
 {
-	//t_map	my_map;
-		long	i[2];
+	long	i[2];
 
 	my_map->strlen = 0;
 	my_map->arrlen = lines_number(ac, av, &my_map->strlen);
-//	ft_printf("In create struct\nStrlen: %d, Arrlen: %d\n", my_map->strlen, my_map->arrlen);// temp
 	my_map->map = ft_calloc(sizeof(int *), (my_map->arrlen + 1));
 	if (!my_map->map)
 		return (0);
@@ -39,7 +37,7 @@ int	create_struct(int ac, char *av[], t_map *my_map)
 	return (1);
 }
 
-static void	free_split(char **arr)
+/* static void	free_split(char **arr)
 {
 	int	j;
 
@@ -52,12 +50,55 @@ static void	free_split(char **arr)
 		j++;
 	}
 	free(arr);
+} */
+
+t_map	*get_map(int ac, char *av[])
+{
+	t_map	*map;
+	char	*line;
+	char	**str_arr;
+	int		i[3];
+
+	map = NULL;
+	if (!create_struct(ac, av, map))
+		return (NULL);
+	i[0] = open(av[1], O_RDONLY);
+	i[1] = 0;
+	while (i[1] < map->arrlen)
+	{
+		i[2] = -1;
+		line = get_next_line(i[0]);
+		str_arr = ft_split(line, ' ');
+		while (++i[2] < map->strlen)
+			map->map[i[1]][i[2]] = ft_atoi(str_arr[i[2]]);
+		//free_split(str_arr);//something wrong with free!!!
+		free(line);
+		i[1]++;
+	}
+	close(i[0]);
+	return (map);
 }
 
 int	main(int ac, char *av[])
 {
+	t_map	*map;
+
+	map = get_map(ac, av);
+	if(!map)
+		return (1);
+	for(int i = 0; i < map->arrlen; i++)
+	{
+		for(int j = 0; j < map->strlen; j++)
+			ft_printf("%d ", map->map[i][j]);
+		ft_printf("\n");
+	}
+	return (0);
+}
+/* 
+int	main(int ac, char *av[])
+{
 	t_map	map;
-	char	*line = NULL;
+	char	*line;
 	char	**str_arr;
 	int		i[3];
 
@@ -65,26 +106,17 @@ int	main(int ac, char *av[])
 		return (1);
 	i[0] = open(av[1], O_RDONLY);
 	i[1] = 0;
-	while (i[1] < map.arrlen)
+	while (i[1] < map->arrlen)
 	{
 		i[2] = -1;
-		line  = get_next_line(i[0]);
+		line = get_next_line(i[0]);
 		str_arr = ft_split(line, ' ');
-/* 		for(int j = 0; j < map.strlen; j++)//temp
-			ft_printf("%s", str_arr[j]);//temp */
-		//ft_printf("\n");//temp
-		//ft_printf("In map:\n");//temp
-		while (++i[2] < map.strlen)
-		{
-			map.map[i[1]][i[2]] = ft_atoi(str_arr[i[2]]);
-			ft_printf("%d", map.map[i[1]][i[2]]);//temp
-		//	i[2]++;
-		}
-		ft_printf("\n");//temp
+		while (++i[2] < map->strlen)
+			map->map[i[1]][i[2]] = ft_atoi(str_arr[i[2]]);
 		free_split(str_arr);//something wrong with free!!!
 		free(line);
 		i[1]++;
 	}
 	close(i[0]);
 	return (0);
-}
+} */
