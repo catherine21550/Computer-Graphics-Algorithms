677e8521207201ef8f36b6feed5f499ca000796d
Kateryna Huk <khuk@c3r4p4.42vienna.com>
2024-08-14 14:39:47 +0200

    14.08.2024 memory allocation for my map added

--- fdf_get_map.c
+++ fdf_get_map.c
@@ -15,12 +15,28 @@
 int	create_struct(int ac, char *av[])
 {
 	t_map	my_map;
-	size_t	i[2];
+	size_t	i[3];
 
-	i[0] = 0;
-	i[1] = lines_number(ac, av, &i[0]);
-	ft_printf("1: %d, 2: %d\n", i[0], i[1]);
-	return (0);
+	my_map.strlen = 0;
+	i[0] = lines_number(ac, av, &my_map.strlen);
+	ft_printf("1: %d, 2: %d\n", my_map.strlen, i[0]);// temp
+	my_map.map = ft_calloc(sizeof(int *), (i[0] + 1));
+	if (!my_map.map)
+		return (0);
+	i[1] = -1;
+	while (++i[1] < i[0])
+	{
+		my_map.map[i[1]] = ft_calloc(sizeof(int), my_map.strlen);
+		if (!my_map.map[i[1]])
+		{
+			i[2] = -1;
+			while (++i[2] < i[1])
+				free(my_map.map[i[2]]);
+			return (free(my_map.map), 0);
+		}
+	}
+	my_map.map[i[0]] = NULL;
+	return (1);
 }
 
 int	main(int ac, char *av[])
