870565af84f8a31fad165bf88881cd3a0fd95bd8
Triinu Eesmaa <triinu.eesmaa@gmail.com>
2025-02-05 17:09:25 +0100

    checking for empty lines in map

--- srcs/parsing/map_checking.c
+++ srcs/parsing/map_checking.c
@@ -9,6 +9,11 @@ void    char_check(t_data *data)
     j = 0;
     while (data->map[i])
     {
+		if (data->map[i][0] == '\0' || data->map[i][0] != '\n')
+		{
+            cleanup(data);
+            exit_error("Map has empty lines");
+        }
         j = 0;
         while (data->map[i][j] && data->map[i][j] != '\n')
         {
